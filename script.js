4d915e54b9c3d0b3368f4310234f0f823fcdd335
Raazkatwal <raazkatwal5@gmail.com>
2023-12-23 21:37:03 +0545

    added database connectivity in signup page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const eye_icon = document.querySelectorAll(".eye-icon");
-// const password_input = document.querySelector("#password");
+const password_input = document.querySelector("#password");
 const menu_btn = document.querySelector(".fa-bars");
 const menu_close_btn = document.querySelector(".fa-xmark");
 const mbl_nav = document.querySelector(".mbl-nav");
@@ -29,17 +29,13 @@ const swiper = new Swiper('.swiper', {
     main_content.classList.remove("disable-click");
     footer.classList.remove("disable-click");
   })
-  function password_is_null() {
-    password_input.classList.add("input-error");
-    password_input.parentElement.classList.add("password-error");
-    password_input.value = "";
-  }
   function password_input_type_toggle(a) {
     (a.type=='text')? a.type = 'password' : a.type = 'text';
   }
   eye_icon.forEach(e => {
     e.addEventListener('click', ()=>{
+      console.log(password_input.value);            
       e.classList.toggle("fa-eye");
-        password_input_type_toggle(login_password_input);      
+        password_input_type_toggle(password_input);           
     })
   });
--- signup.php
+++ signup.php
@@ -1,18 +1,38 @@
 <!DOCTYPE html>
 <?php
-if (isset($_POST['test'])) 
+$email_null = null;
+$email_used = null;
+$username_null = null;
+$username_used = null;
+$password_null = null;
+if (isset($_POST['submit'])) 
 {
-     $password = $_POST['password'];
-    if (empty($password)) {
-    echo'
-    <script defer>
-    console.log("gadbad hai baba");
-        const password_input = document.querySelector("#password_textbox");
-        password_input.classList.add("input-error");
-        password_input.parentElement.classList.add("password-error");
-        password_input.value = "";
-    </script>
-    ';
+    include 'db.php';
+    $form_email = $_POST['email'];
+    $form_username = $_POST['username'];
+    $form_password = $_POST['password'];
+    if (empty($form_email)) {
+        $email_null = true;
+    }
+    if (empty($form_username)) {
+        $username_null = true;
+    }
+    if (empty($form_password)) {
+        $password_null = true;
+    }
+    $username_check = mysqli_query($con, "SELECT * FROM `userinfo` WHERE `username` = '$form_username'");
+    $email_check = mysqli_query($con, "SELECT * FROM `userinfo` WHERE `username` = '$form_username'");
+    if (mysqli_num_rows($username_check)==0 && mysqli_num_rows($email_check)==0) {
+        $form_password = password_hash($form_password, PASSWORD_DEFAULT);
+        mysqli_query($con, "INSERT INTO `userinfo`(`username`, `email`, `password`) VALUES ('$form_username','$form_email','$form_password')");
+        header('location:index.php');
+    }elseif (mysqli_num_rows($username_check)>0) {
+        $username_used = true;
+        if (mysqli_num_rows($email_check)>0) {
+            $email_used = true;
+        }
+    }else {
+        $email_used = true;
     }
 }
 ?>
@@ -35,23 +55,23 @@ if (isset($_POST['test']))
             Create an account
         </h1>
         <div class="login-section">
-            <div>
+            <div class="<?php if ($email_null == true) {echo 'email-null-error';} if($email_used == true) {echo 'email-error';}?>">
                 <label for="email">Email address*</label>
-                <input type="text" id="email" class="form-input" name="regemail" autocomplete="off">
+                <input type="text" id="email" class="form-input" name="email" autocomplete="off">
             </div>
-            <div>
+            <div class="<?php if ($username_null == true) {echo 'username-null-error';} if($username_used == true) {echo 'username-error';}?>">
                 <label for="username">Username*</label>
                 <input type="text" id="username" class="form-input" name="username" autocomplete="off">
             </div>
-            <div class="password_div">
+            <div class="password_div <?php if ($password_null == true) {echo 'password-error';}?>">
                 <label for="password">Password*</label>
-                <input type="password" name="password" id="password_textbox" class="form-input password-error" autocomplete="off">
+                <input type="password" name="password" id="password" class="form-input" autocomplete="off">
                 <i class="fa-regular fa-eye-slash eye-icon"></i>
             </div>
             <div>
                 <input type="checkbox" id="terms-and-policy"><label for="terms-and-policy"> I agree with all the Terms and conditions</label>
             </div>
-            <input type="submit" value="Register" class="form-btn" name="test">
+            <input type="submit" value="Register" class="form-btn" name="submit">
         </div>
         <p class="linktext">Already have a account ? <a href="login.php">Login here</a></p>
     </form>
