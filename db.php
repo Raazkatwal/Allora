6009155343aa9df9c19a94a7bba7066fd619e7f8
Raaz <raazkatwal5@gmail.com>
2024-07-17 21:34:12 +0545

    changed to laravel project

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'allora';
-    $con = mysqli_connect($server, $username, $password, $dbname);
-?>
