0b4f170045c144f335a79eb0d04261b2a180ab79
Raaz <raazkatwal5@gmail.com>
2024-08-13 14:47:18 +0545

    removed js for modals

--- app/Livewire/ProductsTable.php
+++ app/Livewire/ProductsTable.php
@@ -11,6 +11,16 @@ class ProductsTable extends Component
     public $search = '';
     public $name;
     public $description;
+    public $productId;
+
+    public $isAddModalOpen = false;
+    public $isEditModalOpen = false;
+    public $isDeleteModalOpen = false;
+
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'description' => 'required|string|max:500',
+    ];
     public function render()
     {
         $products = Product::where('name', 'like', '%' . $this->search . '%')
@@ -23,13 +33,66 @@ class ProductsTable extends Component
         ]
     );
     }
-    public function delete(string $id){
-        Product::find($id)->delete();
+    public function showAddModal()
+    {
+        $this->resetModal();
+        $this->isAddModalOpen = true;
     }
-    public function addNewProduct(){
+    public function showEditModal($id)
+    {
+        $this->resetModal();
+        $this->productId = $id;
+        $product = Product::findOrFail($id);
+        $this->name = $product->name;
+        $this->description = $product->description;
+        $this->isEditModalOpen = true;
+    }
+    public function showDeleteModal($id)
+    {
+        $this->resetModal();
+        $this->productId = $id;
+        $this->isDeleteModalOpen = true;
+    }
+    public function closeModal()
+    {
+        $this->resetModal();
+    }
+    public function resetModal()
+    {
+        $this->isAddModalOpen = false;
+        $this->isEditModalOpen = false;
+        $this->isDeleteModalOpen = false;
+
+        $this->reset(['name', 'description', 'productId']);
+    }
+    public function addProduct()
+    {
+        $this->validate();
+
         Product::create([
             'name' => $this->name,
-            'description' => $this->description
+            'description' => $this->description,
         ]);
+        $this->closeModal();
+    }
+    public function viewProduct(int $id){
+
+    }
+    public function updateProduct()
+    {
+        $this->validate();
+
+        $product = Product::find($this->productId);
+        $product->update([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+
+        $this->closeModal();
+    }
+    public function deleteProduct()
+    {
+        Product::destroy($this->productId);
+        $this->closeModal();
     }
 }
--- resources/views/livewire/products-table.blade.php
+++ resources/views/livewire/products-table.blade.php
@@ -1,63 +1,9 @@
 <div class="orders_table">
-    <dialog class="delete_modal">
-        <h1 class="modal-head">Are you sure ?</h1>
-        <p class="modal-text">Are you sure you want to delete this record ?</p>
-        <div class="flex-btns">
-            <button class="table-btn delete-btn" id="modal-delete-btn">Delete</button>
-            <button class="table-btn" id="close-btn">Cancel</button>
-        </div>
-    </dialog>
-    <dialog class="edit_modal">
-        <h1 class="modal-head">Edit</h1>
-        <form action="" class="modal-form">
-            <div>
-                <label for="id">ID</label>
-                <input type="text" name="id" id="id-input" class="modal-form-input" autocomplete="off" value="10" disabled>
-            </div>
-            <div>
-                <label for="name">Name</label>
-                <input type="text" name="name" class="modal-form-input" autocomplete="off">
-            </div>
-            <div>
-                <label for="description">Description</label>
-                <textarea name="" id="modal-textarea" class="modal-form-input"></textarea>
-            </div>
-            <div>
-                <label for="image">Image</label>
-                <input type="file" name="image" class="modal-form-input">
-            </div>
-            <div class="flex-btns">
-                <button class="table-btn edit-btn" id="modal-edit-btn">Edit</button>
-                <button class="table-btn" id="close-btn">Cancel</button>
-            </div>
-        </form>
-    </dialog>
-    <dialog class="add_modal">
-        <h1 class="modal-head">Add</h1>
-        <form action="" class="modal-form">
-            <div>
-                <label for="name">Name</label>
-                <input type="text" name="name" class="modal-form-input" autocomplete="off" wire:model="name">
-            </div>
-            <div>
-                <label for="description">Description</label>
-                <textarea name="" id="modal-textarea" class="modal-form-input" wire:model="description"></textarea>
-            </div>
-            <div>
-                <label for="image">Image</label>
-                <input type="file" name="image" class="modal-form-input">
-            </div>
-            <div class="flex-btns">
-                <button class="table-btn add-btn" id="modal-add-btn"  wire:click.prevent="addNewProduct">Add</button>
-                <button class="table-btn" id="close-btn">Cancel</button>
-            </div>
-        </form>
-    </dialog>
     <h2 class="table_heading">
         <span>Products</span> 
         <div>
             <input wire:model.live.debounce.300ms = "search" type="text" placeholder="Search...." class="searchbar">
-            <button class="table-btn add-btn" id="add-btn">Add <i class="fa-solid fa-plus"></i></button>
+            <button class="table-btn add-btn" wire:click="showAddModal">Add <i class="fa-solid fa-plus"></i></button>
         </div>
     </h2>
     <div class="scrollabe-div">
@@ -68,7 +14,7 @@
                     <th>name</th>
                     <th>id</th>
                     <th>description</th>
-                    <th colspan="3">Status</th>
+                    <th colspan="3">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -79,84 +25,61 @@
                         <td>{{ $p->id }}</td>
                         <td class="shrink-text">{{ $p->description }}</td>
                         <td>
-                            <a href="{{ route('product.view', $p->id)}}"><button class="table-btn view-btn">view</button></a>
+                            <button class="table-btn view-btn" wire:click="viewProduct({{ $p->id }})">view</button>
                         </td>
                         <td>
-                            <button class="table-btn edit-btn" id="edit-btn" data-id="{{ $p->id }}">edit</button>
+                            <button class="table-btn edit-btn" wire:click="showEditModal({{ $p->id }})">edit</button>
                         </td>
                         <td>
-                            <button class="table-btn delete-btn" id="delete-btn" data-id="{{ $p->id }}">delete</button>
+                            <button class="table-btn delete-btn" wire:click="showDeleteModal({{ $p->id }})">delete</button>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+    @if ($isAddModalOpen || $isEditModalOpen)
+    <div class="modal_wrapper">
+        <div class="modal-content">
+            <h1 class="modal-head">{{ $isEditModalOpen ? 'Edit Product' : 'Add Product' }}</h1>
+            <form wire:submit.prevent="{{ $isEditModalOpen ? 'updateProduct' : 'addProduct' }}" class="modal-form">
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" wire:model.defer="name" class="modal-form-input @error('name') input-error @enderror" autocomplete="off" wire:model="name">
+                    <span class="input-error-msg">
+                        @error('name') {{$message}} @enderror
+                    </span>
+                </div>
+                <div>
+                    <label for="description">Description</label>
+                    <textarea name="" id="modal-textarea" wire:model.defer="description" class="modal-form-input @error('description') input-error @enderror" wire:model="description"></textarea>
+                    <span class="input-error-msg">
+                        @error('description') {{$message}} @enderror
+                    </span>
+                </div>
+                <div>
+                    <label for="image">Image</label>
+                    <input type="file" name="image" class="modal-form-input">
+                </div>
+                <div class="flex-btns">
+                    <button type="submit" class="table-btn {{ $isEditModalOpen ? 'edit-btn' : 'add-btn' }}">{{ $isEditModalOpen ? 'Update' : 'Add' }}</button>
+                        <button type="button" wire:click="closeModal" class="table-btn close_btn">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    @endif
+    @if ($isDeleteModalOpen)
+    <div class="modal_wrapper">
+        <div class="modal-content">
+            <h1 class="modal-head">Are you sure ?</h1>
+            <p class="modal-text">Are you sure you want to delete this record ?</p>
+            <div class="flex-btns">
+                <button class="table-btn delete-btn" wire:click="deleteProduct">Delete</button>
+                <button class="table-btn"  wire:click="closeModal">Cancel</button>
+            </div>
+        </div>
+    </div>
+    @endif
 </div>
-@push('scripts')
-<script>
-    const delete_modal = document.querySelector(".delete_modal");
-    const add_modal = document.querySelector(".add_modal");
-    const edit_modal = document.querySelector(".edit_modal");
-    const table_delete_btn = document.querySelectorAll("#delete-btn");
-    const table_add_btn = document.querySelector("#add-btn");
-    const table_edit_btn = document.querySelectorAll("#edit-btn");
-    const modal_close_btn = document.querySelectorAll("#close-btn");
-    const modal_delete_btn = document.querySelector('#modal-delete-btn');
-    let productId = null;
-
-    table_delete_btn.forEach(e => {
-        e.addEventListener('click', () => {
-            productId = e.getAttribute('data-id');
-            delete_modal.showModal();
-        });
-    });
-
-    table_add_btn.addEventListener('click', () => {
-        add_modal.showModal();
-    });
-
-    modal_close_btn.forEach(e => {
-        e.addEventListener('click', e1 => {
-            e1.preventDefault();
-            closeModals();
-        });
-    });
-
-    modal_delete_btn.addEventListener('click', () => {
-        if (productId) {
-            @this.delete(productId);
-            closeModals();
-        }
-    });
-
-    table_edit_btn.forEach(e => {
-        e.addEventListener('click', () => {
-            productId = e.getAttribute('data-id');
-            edit_modal.showModal();
-        });
-    });
-
-    function closeModals() {
-        delete_modal.close();
-        edit_modal.close();
-        add_modal.close();
-    }
-    // Listen for the event emitted after adding a product
-    document.addEventListener('livewire:init', () => {
-       Livewire.on('delete', () => {
-            console.log("kaam vayo");
-        
-            closeModals();
-       });
-    });
-
-    // Ensure modals are properly closed if clicked outside
-    window.addEventListener('click', (e) => {
-        if (e.target === delete_modal || e.target === add_modal || e.target === edit_modal) {
-            closeModals();
-        }
-    });
-</script>
-@endpush
 
